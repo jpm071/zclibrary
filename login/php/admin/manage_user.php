6d6f8d7b069c8952c0d4bd41b22d9267666f1782
John Paul <Potitoys4@gmail.com>
2022-03-28 01:55:19 +0800

    Third Commit

--- login/php/admin/manage_user.php
+++ login/php/admin/manage_user.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <link rel="shortcut icon" href="../zc_seal.png"/>
+    <link rel="shortcut icon" href="/consulta/images/zc_seal.png"/>
       <title>User Management | ZC Library</title>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
       integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
